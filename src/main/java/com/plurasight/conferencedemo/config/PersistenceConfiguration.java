691dc2fb3850c77955ad90a5e4598c9b460c7d6b
Tom Zhang <tianze.zhang@gmail.com>
2022-06-19 02:33:51 +0100

    final changes

--- src/main/java/com/plurasight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/plurasight/conferencedemo/config/PersistenceConfiguration.java
@@ -3,18 +3,25 @@ package com.plurasight.conferencedemo.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    /*@Bean
+    @Bean
     public DataSource dataSource(){
         DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.username("root");
-        builder.password("tianze2001");
+        builder.username("");
+        builder.password("");
         builder.url("jdbc:mysql://localhost:3306/conference_demo");
         return builder.build();
-    }*/
+    }
+
+
 
 }
+
+
